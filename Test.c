290b60abd0591ea8f4071e17d0042ed272bb46b4
RyanSciarabba <rhsciarabba@gmail.com>
2018-09-05 11:24:19 -0400

    Documented

--- Test.c
+++ Test.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
 int main() {
+	
+	//Define variables
 	int a;
 	int b;
 	int c;
+
+	//Ask for integer A
 	printf("Input an integer >>> ");
 	scanf("%i", &a);
+
+	//Ask for integer B
 	printf("Input a second integer >>> ");
 	scanf("%i", &b);
+
+	//Print out A and B then sum both for C
 	printf("A: %i\nB: %i\n\n", a, b);
 	c = a + b;
 	printf("Sum: %i\n", c); 
+
+	//End program
 	return 0;
 }
