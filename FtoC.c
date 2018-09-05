84c878a947fd998a457b5d16a242da76967432de
Ryan-Sciarabba <rhsciarabba@gmail.com>
2018-09-05 12:54:00 -0400

    Fixed FtoC

--- FtoC.c
+++ FtoC.c
@@ -4,25 +4,19 @@
 int main() {
 	
 	//Define Variables for Fahrenheit = F, Celsius = C, and the fraction in the formula;
-	double F;
-	double C;
-	double Fraction = 5.0/9.0;
-	double Last;
+	float F;
+	float C;
 	
 	//Get the temperature in Fahrenheit from user
 	printf("Input temperature in Fahrenheit >>> ");
-	scanf("%1f", &F);
-	printf("\nF = %1f", F);
+	scanf("%f", &F);
 
 	//Formula to turn Fahrenheit into Celsius
-	Last = F - 32.0;
-	printf("\nLast = %1f", Last);
-	C = Fraction * (F - 32.0);
+	C = (5.0/9.0) * (F - 32.0);
 
 	//Print the temperature in Fahrenheit and in Celsius
-	printf("\nFraction = %1f", Fraction);
-	printf("\nFahrenheit: %d \n", F);
-	printf("Celsius: %1f", C);
+	printf("\nFahrenheit: %f\n", F);
+	printf("Celsius: %f", C);
 
 	return 0;
 }
