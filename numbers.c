5b46288684b6e9d352207a72e0243f8f923bec70
RyanSciarabba <rhsciarabba@gmail.com>
2018-11-05 16:16:10 -0500

    Fixed numbers.c

--- numbers.c
+++ numbers.c
@@ -2,107 +2,147 @@
    Purpose: To take in a number and return some information about it
    Written by: Ryan Sciarabba
    Language: c (gcc target)
-   Version Number: 1.5.7
+   Version Number: 1.6.9
    Date of Creation: October 5th, 2018
-   Date of Last Revision: October 22th, 2018
+   Date of Last Revision: November 5th, 2018
 */
 
 #include <stdio.h>
 
+//Prototype for the calculation function
+void calculations(double *mSeven, double *mEleven, double *mThirteen, int *sum, double *prime);
+	
 int main(void){
 	
-	int number, //Given number
-		 c, //Number of numbers to be input
-		 x, //The current number in c 
-	   mNumber, //The multiple of the number 
-	       sum,
-	       rem; //The remainder in a later calculation
-
-	double mSeven, //Multiple of seven
-	      mEleven, //Multiple of eleven 
-	    mThirteen, //Multiple of thirteen 
-		    i, //Iterator for a later loop 
-		prime; //Check to see if the number is prime
 	
-	//Set c to y for the continuation of the code
+	int c, //number of numbers to be input
+	    x, //counter for while loop
+	  sum; //sum of digits
+
+	double mSeven, //multiple of seven
+	      mEleven, //multiple of eleven
+	    mThirteen, //multiple of thirteen
+		prime; //prime number
+	
+	//See how many numbers will be input
 	printf("How many numbers will be input? >>> ");
 	scanf("%i", &c);
 	
-	//Set i to 2 for the for the prime number calculation later
-	i = 2.0;
+	//Set x to zero for while loop
 	x = 0;
 	
-	
-	//While loop to keep prompting for more numbers
+	//repeat for the number of numbers to be input
 	while(x < c){
-		
-		//Take in a number
-		printf("Input a positive whole number (Ex. 7) >>> ");
-		scanf("%i", &number);
-		printf("\n");
-		
-		//Calculate multiples
-		mSeven = number / 7.0;
-		mEleven = number / 11.0;
-		mThirteen = number / 13.0;
-		if(number != 0){
-			
-			//Check to see if the number is a multiple of 7, 11, 13
-			if((int)mSeven == mSeven){
-				printf("Is %i a multiple of 7: Yes\n", number);
-			}
-			else{
-				printf("Is %i a mupltiple of 7: No\n", number);
-			}
-			if((int)mEleven == mEleven){
-				printf("Is %i a multiple of 11: Yes\n", number);
-			}
-			else{
-				printf("Is %i a multiple of 11: No\n", number);
-			}
-			if((int)mThirteen == mThirteen){
-				printf("Is %i a multiple of 13: Yes\n", number);
-			}
-			else{
-				printf("Is %i a multiple of 13: No\n", number);
-			}
-			
-			//Find the sum of the digits
-			rem = number;			
-			sum = 0;	
-		
-			while(rem != 0){
-				sum = rem % 10;
-				rem = rem / 10;
-			}
-
-			//Check to see if the sum is odd or even
-			if(sum % 2 == 0){
-				printf("Sum of digits is even: %i\n", sum);
-			}
-			else{
-				printf("Sum of digits is odd: %i\n", sum);
-			}
+		calculations(&mSeven, &mEleven, &mThirteen, &sum, &prime);
+		if(mSeven == 1){
+			printf("Number is a multiple of 7\n");
 		}
-
-		//Check to see if the number is prime
-		for(i = 2.0; i <= number; i++){
-			prime = number / i;
-			if(number == i){
-				printf("%i is a prime number\n\n", number);
-				break;
-			}
-			if((int)prime == prime){
-				printf("%i is not prime\n\n", number);
-				break;
-			}
-			else{
-				continue;
-			} 
+		else{
+			printf("Number is not a multiple of 7\n");
 		}
-
-		//Add 1 to the number of input and checked numbers
-		x = x + 1;
+		if(mEleven == 1){
+			printf("Number is a multiple of 11\n");
+		}
+		else{
+			printf("Number is not a multiple of 11\n");
+		}
+		if(mThirteen == 1){
+			printf("Number is a multiple of 13\n");
+		}
+		else{
+			printf("Number is not a multiple of 13\n");
+		}
+		if(sum == 1){
+			printf("Sum of the digits is even\n");
+		}
+		else{
+			printf("Sum of the digits is odd\n");
+		}
+		if(prime == 1){
+			printf("Number is prime\n");
+		}
+		else{
+			printf("Number is not prime\n");
+		}
+		x++;
 	}
-	return 0;
+}
+
+void calculations(double *mSeven, double *mEleven, double *mThirteen, int *sum, double *prime){
+	
+	int number, //Given number
+		 x, //The current number in c 
+	   mNumber, //The multiple of the number 
+	       rem; //The remainder in a later calculation
+
+  	double i; //Iterator for a later loop 
+	
+	
+	//Set i to 2 for the for the prime number calculation later
+	i = 2.0;
+		
+	//Take in a number
+	printf("Input a positive whole number (Ex. 7) >>> ");
+	scanf("%i", &number);
+	printf("\n");
+		
+	//Calculate multiples
+	*mSeven = number / 7.0;
+	*mEleven = number / 11.0;
+	*mThirteen = number / 13.0;
+	if(number != 0){
+			
+		//Check to see if the number is a multiple of 7, 11, 13
+		if((int)*mSeven == *mSeven){
+			*mSeven = 1;
+		}
+		else{
+			*mSeven = 0;
+		}
+		if((int)*mEleven == *mEleven){
+			*mEleven = 1;
+		}
+		else{
+			*mEleven = 0;
+		}
+		if((int)*mThirteen == *mThirteen){
+			*mThirteen = 1;
+		}
+		else{
+			*mThirteen = 0;
+		}
+			
+		//Find the sum of the digits
+		rem = number;			
+		*sum = 0;	
+		
+		while(rem != 0){
+			*sum = rem % 10;
+			rem = rem / 10;
+		}
+
+		//Check to see if the sum is odd or even
+		if(*sum % 2 == 0){
+			*sum = 1;
+		}
+		else{
+			*sum = 0;
+		}
+	}
+
+	//Check to see if the number is prime
+	for(i = 2.0; i <= number; i++){
+		*prime = number / i;
+		if(number == i){
+			*prime = 1;
+			break;
+		}
+		if((int)*prime == *prime){
+			*prime = 0;
+			break;
+		}
+		else{
+			continue;
+		} 
+	}
 }
